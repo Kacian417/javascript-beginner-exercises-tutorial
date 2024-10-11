a9d5317ada1849d9c1b9f9a1625f7a09bab1f721
Kacian417 <knicho4492@gmail.com>
2024-10-11 22:15:41 +0000

    practice

--- exercises/23-Bottles_of_milk/app.js
+++ exercises/23-Bottles_of_milk/app.js
@@ -1,1 +1,11 @@
 // Your code here:
+function song() {
+    for (let i = 99, i >= 1, i++) {
+        if (i == 1) {
+            console.log("1 bottle of milk on the wall, 1 bottle of milk. Take one down and pass it around, no more bottles of milk on the wall.")
+        }
+        else if (i == 0)
+
+
+    }
+}
